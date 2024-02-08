c933500cf6c8c657663e66cf5c835f37dadbe5fa
Rémi LAFORGUE <laforgueremi@gmail.com>
2024-02-08 09:57:10 +0100

    jdtls config added

---
+++ after/plugin/lsp-zero.lua
@@ -0,0 +1,11 @@
+local lsp = require('lsp-zero').preset({})
+
+-- lsp.on_attach(function(client, bufnr)
+--   -- see :help lsp-zero-keybindings
+--   -- to learn the available actions
+--   lsp.default_keymaps({buffer = bufnr})
+-- end)
+
+lsp.skip_server_setup({'jdtls'})
+
+lsp.setup()
