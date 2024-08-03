ff8c9bd2c1aa376a5fc3d052e0c500a1e5928f2a
Remi LAFORGUE <laforgue.remi@gmail.com>
2024-08-03 14:26:06 +0200

    disabled treesitter indent to fix indent pb

--- lua/remi/set.lua
+++ lua/remi/set.lua
@@ -9,6 +9,8 @@ opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 2 spaces for indent width
 -- opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true
+opt.smarttab = true
 
 -- line wrapping
 opt.wrap = true -- disable line wrapping
@@ -18,7 +20,7 @@ opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = false -- highlight the current cursor line
+opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
 
@@ -27,6 +29,8 @@ opt.cursorline = false -- highlight the current cursor line
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+opt.colorcolumn = "80"
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
