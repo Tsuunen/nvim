c8a77c0d29a8f0cc5624fa9a1700ddf414234e5a
Remi LAFORGUE <laforgue.remi@gmail.com>
2025-05-14 18:22:33 +0200

    emmet lsp added

--- lua/Tsuunen/init.lua
+++ lua/Tsuunen/init.lua
@@ -1,2 +1,16 @@
 require("Tsuunen.set")
 require("Tsuunen.lazy")
+require'lspconfig'.emmet_ls.setup({
+  filetypes = {
+    'html', 'css', 'php', 'javascriptreact', 'typescriptreact',
+    'vue', 'svelte', 'xml'
+  },
+  init_options = {
+    html = {
+      options = {
+        ['bem.enabled'] = true,
+      },
+    },
+  }
+})
+
